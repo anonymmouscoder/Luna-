522bbff7e9cbc618f23051f1d01563eeeb3f3ec6
Liku cs <83144124+likucs@users.noreply.github.com>
2021-07-02 02:47:21 +0530

    Update luna.py

--- luna.py
+++ luna.py
@@ -32,129 +32,50 @@ async def type_and_send(message):
 @luna.on_message(filters.command("repo") & ~filters.edited)
 async def repo(_, message):
     await message.reply_text(
-        "[Github](https://github.com/thehamkercat/LunaChatBot)"
-        + " | [Group](t.me/PatheticProgrammers)", disable_web_page_preview=True)
-
-@luna.on_message(filters.command("help") & ~filters.edited)
-async def start(_, message):
-    user_id = message.from_user.id
-    if user_id in blacklisted:
-        return
-    await luna.send_chat_action(message.chat.id, "typing")
-    await message.reply_text(
-        "**Only For Owners**\n/shutdown - `Shutdown Luna.`\n/blacklist - `Blacklist A User.`\n/whitelist - `Whitelist A User.`"
+        "[GitHub](https://github.com/thehamkercat/LunaChatBot)"
+        + " | [Group](t.me/PatheticProgrammers)",
+        disable_web_page_preview=True,
     )
 
 
-@luna.on_message(filters.command("shutdown") & filters.user(owner_id) & ~filters.edited)
-async def shutdown(_, message):
+@luna.on_message(filters.command("help") & ~filters.edited)
+async def start(_, message):
     await luna.send_chat_action(message.chat.id, "typing")
-    await message.reply_text("**Shutted Down!**")
-    print("Exited!")
-    exit()
-
-
-@luna.on_message(filters.command("blacklist") & filters.user(owner_id) & ~filters.edited)
-async def blacklist(_, message):
-    global blacklisted
-    if not message.reply_to_message:
-        await luna.send_chat_action(message.chat.id, "typing")
-        await message.reply_text("Reply To A User's Message To Blacklist.")
-        return
-    victim = message.reply_to_message.from_user.id
-    if victim in blacklisted:
-        await luna.send_chat_action(message.chat.id, "typing")
-        await message.reply_text("Already Blacklisted Lol.")
-        return
-    blacklisted.append(victim)
-    await luna.send_chat_action(message.chat.id, "typing")
-    await message.reply_text("Blacklisted.")
-
-
-@luna.on_message(filters.command("whitelist") & filters.user(owner_id) & ~filters.edited)
-async def whitelist(_, message):
-    global blacklisted
-    if not message.reply_to_message:
-        await luna.send_chat_action(message.chat.id, "typing")
-        await message.reply_text("Reply To A User's Message To Whitelist.")
-        return
-    victim = message.reply_to_message.from_user.id
-    if victim not in blacklisted:
-        await luna.send_chat_action(message.chat.id, "typing")
-        await message.reply_text("Already Whitelisted Lol.")
-        return
-    blacklisted.remove(victim)
-    await luna.send_chat_action(message.chat.id, "typing")
-    await message.reply_text("Whitelisted.")
+    await sleep(2)
+    await message.reply_text("/repo - Get Repo Link")
 
 
 @luna.on_message(
-    ~filters.private &
-    ~filters.command("blacklist")
-    & ~filters.command("shutdown")
+    ~filters.private
+    & filters.text
     & ~filters.command("help")
-    & ~filters.edited
+    & ~filters.edited,
+    group=69
 )
 async def chat(_, message):
-    if message.from_user.id in blacklisted:
-        return
     if message.reply_to_message:
-        if not message.reply_to_message.from_user.id == bot_id:
+        if not message.reply_to_message.from_user:
             return
-        await luna.send_chat_action(message.chat.id, "typing")
-        if not message.text:
-            query = "Hello"
-        else:
-            query = message.text
-        if len(query) > 50:
+        from_user_id = message.reply_to_message.from_user.id
+        if from_user_id != bot_id:
             return
-        try:
-            res = await getresp(query)
-            await asyncio.sleep(1)
-        except Exception as e:
-            res = str(e)
-        await message.reply_text(res)
-        await luna.send_chat_action(message.chat.id, "cancel")
-    else:
-        if message.text:
-            query = message.text
-            if len(query) > 50:
-                return
-            if re.search("[.|\n]{0,}[l|L][u|U][n|N][a|A][.|\n]{0,}", query):
-                await luna.send_chat_action(message.chat.id, "typing")
-                try:
-                    res = await getresp(query)
-                    await asyncio.sleep(1)
-                except Exception as e:
-                    res = str(e)
-                await message.reply_text(res)
-                await luna.send_chat_action(message.chat.id, "cancel")
+    match = re.search("[.|\n]{0,}luna[.|\n]{0,}", message.text.strip(), flags=re.IGNORECASE)
+    if not match and from_user_id != bot_id:
+        return
+    await type_and_send(message)
 
 
 @luna.on_message(
     filters.private
-    & ~filters.command("blacklist")
-    & ~filters.command("shutdown")
     & ~filters.command("help")
     & ~filters.edited
 )
 async def chatpm(_, message):
-    if message.from_user.id in blacklisted:
-        return
     if not message.text:
         return
-    await luna.send_chat_action(message.chat.id, "typing")
-    query = message.text
-    if len(query) > 50:
-        return
-    try:
-        res = await getresp(query)
-        await asyncio.sleep(1)
-    except Exception as e:
-        res = str(e)
-    await message.reply_text(res)
-    await luna.send_chat_action(message.chat.id, "cancel")
+    await type_and_send(message)
 
+luna.start()
 
 print(
     """
@@ -163,6 +84,4 @@ print(
 -----------------
 """
 )
-
-
-luna.run()
+idle()
