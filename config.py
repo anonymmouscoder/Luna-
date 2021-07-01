d65819a4130fa6cc3b76a20b33d0c90c5b3eede1
Liku cs <83144124+likucs@users.noreply.github.com>
2021-07-01 16:56:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 owner_id = "1340617370"
 bot_token = "1797048582:AAEycxgHMhJKs3TXgAUaR9n23Ctbsnv-JmA"
 bot_id = "1797048582"
-
+ARQ_API_BASE_URL = "https://thearq.tech"
 # Leave it as it is
 ARQ_API_BASE_URL = "https://thearq.tech"
